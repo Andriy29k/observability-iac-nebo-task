67a7ec08c8bbba76ab6469ebfb7533e5a854b786
Andrii Korotchuk <akorot@softserveinc.com>
2026-05-19 13:32:16 +0300

    Formatted terraform files using terraform fmt

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -19,13 +19,13 @@ output "vm_name" {
 }
 
 output "vm_public_ip" {
-  value = azurerm_public_ip.observability-task-vm-pip.ip_address
+  value = azurerm_public_ip.observability-task-public-ip.ip_address
 }
 
 output "ssh_to_vm" {
-  value = "ssh ${var.admin_username}@${azurerm_public_ip.observability-task-vm-pip.ip_address} -i ${var.ssh_key_path}"
+  value = "ssh ${var.admin_username}@${azurerm_public_ip.observability-task-public-ip.ip_address} -i ${var.ssh_key_path}"
 }
 
 output "url_to_app" {
-  value = "http://${azurerm_public_ip.observability-task-vm-pip.ip_address}:5000"
+  value = "http://${azurerm_public_ip.observability-task-public-ip.ip_address}:5000"
 }
