2e121377775e779a7ac9dae255936f7b9545b905
Andrii Korotchuk <akorot@softserveinc.com>
2026-05-19 13:17:33 +0300

    Wrote Terraform code

---
+++ terraform/variables.tf
@@ -0,0 +1,64 @@
+variable "subscription" {
+    type = string
+}
+
+variable "resource_group_name" {
+    type    = string
+    default = "observability-task-rg"
+}
+
+variable "location" {
+    type = string
+}
+
+variable "vnet_name" {
+    type = string
+}
+
+variable "vnet_address_space" {
+    type = string
+}
+
+variable "public_subnet_address_prefix" {
+    type = string
+}
+
+variable "vm_name" {
+    type = string
+}
+
+variable "vm_size" {
+    type = string
+}
+
+variable "admin_username" {
+    type = string
+}
+
+variable "ssh_key_path" {
+    type = string
+}
+
+variable "publisher" {
+    type = string
+}
+
+variable "offer" {
+    type = string
+}
+
+variable "sku" {
+    type = string
+}
+
+variable "image_version" {
+    type = string
+}
+
+variable "storage_account_type" {
+    type = string
+}
+
+variable "caching_type" {
+    type = string
+}
